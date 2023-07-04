8543d2e7bd6e35350b45eb7887bf6027097259f3
unknown <milanzec53@gmail.com>
2023-07-04 20:50:10 +0200

    cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import CurrentEvent from "./components/CurrentEvent";
 
 import { fetchData, eventOptions } from "./utils/fetchData";
 
 function App() {
-  const [fighters, setFighters] = useState([]);
+  const [currentEventData, setCurrentEventData] = useState([]);
 
   useEffect(() => {
     const fetchDataFromApi = async () => {
@@ -12,7 +13,7 @@ function App() {
         const url = "https://mma-stats.p.rapidapi.com/July_01_2023";
         const data = await fetchData(url, eventOptions);
         console.log(data);
-        setFighters(data);
+        setCurrentEventData(data);
       } catch (error) {
         console.error(error);
       }
@@ -21,22 +22,10 @@ function App() {
     fetchDataFromApi();
   }, []);
 
-  if (!Array.isArray(fighters)) {
-    return <p>No fighters data available.</p>;
-  }
-
   return (
     <>
       <h1>UFC Stats Project</h1>
-      <ul>
-        {fighters.map((fighter, index) => (
-          <li key={index}>
-            <h2>Fighter {index + 1}</h2>
-            <p>Matchup: {fighter.matchup.join(" vs ")}</p>
-            <p>Tale of the Tape: {JSON.stringify(fighter.tale_of_the_tape)}</p>
-          </li>
-        ))}
-      </ul>
+      <CurrentEvent data={currentEventData}/>
     </>
   );
 }
--- src/components/CurrentEvent.jsx
+++ src/components/CurrentEvent.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Typography, Grid, Card, CardContent } from "@mui/material";
 
-const CurrentEvent = () => {
+const CurrentEvent = ({ data }) => {
   return (
-    <div>CurrentEvent</div>
-  )
-}
+    <div style={{ margin: "16px" }}>
+      <Typography variant="h1" style={{ marginBottom: "16px" }}>
+        Current Event
+      </Typography>
+      <Grid container spacing={2}>
+        {data.map((fighter, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Card>
+              <CardContent>
+                <Typography
+                  variant="h5"
+                  style={{ marginBottom: "8px", textAlign: "center" }}
+                >
+                  {index === 0 ? "Main Fight" : `Fight ${index}`}
+                </Typography>
+                <Typography variant="body1" style={{ marginBottom: "8px" }}>
+                  {fighter.matchup.join(" vs ")}
+                </Typography>
+                <Typography variant="body2">
+                  Reach: {fighter.tale_of_the_tape.reach}
+                </Typography>
+                <Typography variant="body2">
+                  Record: {fighter.tale_of_the_tape.record}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
 
-export default CurrentEvent
+export default CurrentEvent;
+
+
